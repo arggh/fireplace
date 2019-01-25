8a4d85df2df44b5770cf95eae2fa7929079e51ab
arggh <arggh@users.noreply.github.com>
2019-01-25 12:00:51 +0200

    Bump version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'arggh:fireplace',
-  version: '0.0.1',
+  version: '0.0.2',
   summary: 'Fireplace is a minimalist lightbox component for Blaze',
   git: 'https://github.com/arggh/fireplace',
   documentation: 'README.md'
