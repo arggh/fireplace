725776e1b92b283b49935308b63b4886499d4964
arggh <arggh@users.noreply.github.com>
2019-08-04 23:02:45 +0300

    CSS fix

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'arggh:fireplace',
-  version: '0.0.7',
+  version: '0.0.8',
   summary: 'Fireplace is a minimalist lightbox component for Blaze',
   git: 'https://github.com/arggh/fireplace',
   documentation: 'README.md'
