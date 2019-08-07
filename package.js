64a32ba7b23672d81170caef1f7ea1219c4dd457
arggh <arggh@users.noreply.github.com>
2019-08-07 10:37:36 +0300

    Switch focus shadow to inset
    Fixes a rendering glitch in Safari

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'arggh:fireplace',
-  version: '0.0.13',
+  version: '0.0.14',
   summary: 'Fireplace is a minimalist lightbox component for Blaze',
   git: 'https://github.com/arggh/fireplace',
   documentation: 'README.md'
