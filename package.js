64c9fba214d3dac6bd9a220186261d27b55fc433
arggh <arggh@users.noreply.github.com>
2019-08-07 10:28:22 +0300

    Fix image fit issue with Safari

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'arggh:fireplace',
-  version: '0.0.12',
+  version: '0.0.13',
   summary: 'Fireplace is a minimalist lightbox component for Blaze',
   git: 'https://github.com/arggh/fireplace',
   documentation: 'README.md'
