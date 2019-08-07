b8d61e542fe59cd93e708d47bec2d1e5734ecd84
arggh <arggh@users.noreply.github.com>
2019-08-07 09:58:35 +0300

    Tweaks to make the new layout work on FF + Safari

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'arggh:fireplace',
-  version: '0.0.9',
+  version: '0.0.10',
   summary: 'Fireplace is a minimalist lightbox component for Blaze',
   git: 'https://github.com/arggh/fireplace',
   documentation: 'README.md'
