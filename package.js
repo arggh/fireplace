cc078127b683f3949f5ee13100b8daa1ea8a658a
arggh <arggh@users.noreply.github.com>
2019-10-16 22:31:36 +0300

    Fix centering issue on large displays

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'arggh:fireplace',
-  version: '0.0.16',
+  version: '0.0.17',
   summary: 'Fireplace is a minimalist lightbox component for Blaze',
   git: 'https://github.com/arggh/fireplace',
   documentation: 'README.md'
